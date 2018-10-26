cb6f5b88de20a4f1cab0645bcf2bc287bc8055d1
Александр <shlykovn@mail.ru>
2018-10-26 23:56:28 +0300

    works

--- backend/modules/works/views/works/_form.php
+++ backend/modules/works/views/works/_form.php
@@ -20,6 +20,8 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
+    <?= $form->field($model, 'image')->textInput(['maxlength' => true]) ?>
+
     <div class="form-group">
         <?= Html::submitButton(Yii::t('works', 'Save'), ['class' => 'btn btn-success']) ?>
     </div>

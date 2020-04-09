e4e9424b78734ca6e751abcf52b64bf937ddeb19
Kavalar <apuc06@mail.ru>
2020-04-09 17:04:24 +0300

    Update SiteMap

--- frontend/views/sitemap/index.php
+++ frontend/views/sitemap/index.php
@@ -5,14 +5,12 @@
 echo '<?xml version="1.0" encoding="UTF-8"?>';
 ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-    <url>
         <?php foreach ($siteMaps as $siteMap): ?>
-            <sitemap>
+    <url>
                 <loc><?= Yii::$app->request->hostInfo.'/'.$siteMap['loc'] ?></loc>
                 <lastmod><?= $siteMap['lastmod'] ?></lastmod>
                 <changefreq><?= $siteMap['changefreq'] ?></changefreq>
                 <priority><?= $siteMap['priority'] ?></priority>
-            </sitemap>
-        <?php endforeach; ?>
     </url>
+        <?php endforeach; ?>
 </urlset>

9c713b38770ca65c7b67c1a5e681588b4b9629cc
serezha10x <kanatush.lorraine01@gmail.com>
2020-09-03 15:11:35 +0300

    after error merge

---
+++ backend/modules/balancecash/models/BalanceCash.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace backend\modules\balancecash\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "balance_cash".
+ *
+ * @property int $id
+ * @property int|null $user_id
+ * @property int|null $amount
+ */
+class BalanceCash extends \common\models\BalanceCash
+{
+
+}
---
+++ common/models/BalanceCash.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "balance_сash".
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $amount
+ */
+class BalanceCash extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'balance_cash';
+    }
+
+    /**
+     * @param $user
+     * @param $amount
+     */
+    public static function create($user,$amount)
+    {
+        $balance = new self;
+        $balance->user_id = $user;
+        $balance->amount = $amount;
+        $balance->save();
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['user_id', 'amount'], 'integer'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('balance', 'ID'),
+            'user_id' => 'Пользователь',
+            'amount' => Yii::t('balance', 'Amount'),
+        ];
+    }
+
+    /**
+     * @param $amount
+     * @return boolean
+     */
+    public function addBalance($amount)
+    {
+
+       $this->amount += (integer)$amount;
+       $this->save();
+
+       return true;
+    }
+
+    /**
+     * @param $likes
+     * @param $views
+     * @return bool
+     */
+    public function removeFromBalance($amount)
+    {
+
+        $this->amount -= (integer)$amount;
+        $this->save();
+
+        return true;
+    }
+
+
+
+    public function getUser()
+    {
+        return $this->hasOne(User::className(),['id'=>'user_id']);
+    }
+}
